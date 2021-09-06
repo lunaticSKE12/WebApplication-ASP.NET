b89015beba67c2facf5a6754e7ae11e1220f8840
lunatic <lunatic@NAPONGDUNGD5FD4>
2021-09-06 16:09:08 +0700

    Add project files.

---
+++ WebApplication1/Home.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="WebApplication1.Home" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Image ID="Image1" runat="server" Height="133px" ImageUrl="~/TheTwist_EN-US1554358364_1920x1200.jpg" style="margin-top: 0px" Width="240px" />
+            <br />
+            ปี ค.ศ. เกิด<asp:TextBox ID="txtYears" runat="server" OnTextChanged="TextBox1_TextChanged"></asp:TextBox>
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="เข้าสู่ระบบ" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/Home.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (ReadCookie() >= 15)
+            {
+                // Server.Transfer("Member.aspx");
+                Response.Redirect("Member.aspx");
+            }
+            else
+            {
+                Response.Write("Error !!");
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int years = DateTime.Now.Year;
+            int age = years - int.Parse(txtYears.Text);
+            WriteCookie(age);
+            if (ReadCookie() >= 15)
+            {
+                // Server.Transfer("Member.aspx");
+                Response.Redirect("Member.aspx");
+            }
+            else
+            {
+                Response.Write("Error !!");
+            }
+        }
+
+        private void WriteCookie(int age)
+        {
+            HttpCookie userCookie = new HttpCookie("UserSetting");
+            userCookie.Value = age.ToString();
+            userCookie.Expires = DateTime.Now.AddDays(3d);
+            Response.Cookies.Add(userCookie);
+        }
+
+
+        private int ReadCookie()
+        {
+            HttpCookie userCookies = Request.Cookies["UserSetting"];
+            return int.Parse(userCookies.Value);
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
